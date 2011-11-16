d50cb6203eeb5821c7b6cce1e44cb4ad5353dd1a
Susheel Yadagiri <syadagir@codeaurora.org>
2011-11-16 15:00:21 -0800

    external/iproute2: Add reference count for Smart Wireless Interface Manager
    Change-Id: I665be89cfad616c743dee65d135632f6b695c7cf

--- include/netinet/tcp.h
+++ include/netinet/tcp.h
@@ -192,6 +192,7 @@ struct tcp_info
   u_int8_t	tcpi_backoff;
   u_int8_t	tcpi_options;
   u_int8_t	tcpi_snd_wscale : 4, tcpi_rcv_wscale : 4;
+  u_int8_t»     tcpi_count;
 
   u_int32_t	tcpi_rto;
   u_int32_t	tcpi_ato;
